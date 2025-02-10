26359627270ba6d6afee42ad3057d0b645782e35
Kelly J <[kelly4410@gmail.com]>
2025-02-09 17:15:36 -0700

    update inventory

--- InventoryItem.js
+++ InventoryItem.js
@@ -1,9 +1,27 @@
-function InventoryItem({ name, type, quantity, price }) {
+function InventoryItem({ name, type, quantity = 0, price = 0 }) {
+
+    const lowStockThreshold = 5;
+    const highValueThreshold = 1000;
+    const totalValue = price * quantity;
+
     return (
         <li>
             <h2>{name} ({type})</h2>
-            <p>{quantity < 5 ? "⚠️ Low Stock! Only " + quantity + " left!" : null}</p>
-            <p>{price > 1000 ? "💎 High Value - consider extra protection!" : null} </p>
+            {
+                quantity < lowStockThreshold
+                &&
+                <Message>
+                    <p><span>⚠️</span> Low Stock! Only {quantity} left!</p>
+                </Message>
+            }
+            {
+                totalValue > highValueThreshold
+                &&
+                <Message>
+                    <p><span>💎</span> High Value - consider extra protection!</p>
+                </Message>
+            }
+
         </li>
     );
 }
@@ -13,3 +31,7 @@ function InventoryItem({ name, type, quantity, price }) {
 /* /* <!-- TODO: Render the low stock alert based on the quantity of the item. -->
 <!-- TODO: Render the high value alert based on the total value of the item. --> */
 
+
+
+/* <p>{quantity < 5 ? "⚠️ Low Stock! Only " + quantity + " left!" : null}</p>
+<p>{price > 1000 ? "💎 High Value - consider extra protection!" : null} </p> */
